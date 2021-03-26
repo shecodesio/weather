cda7f4315a5d9a8dda3f8597412ae6fce285d983
Matt Delac <matthieu.aussaguel@gmail.com>
2021-03-26 09:58:12 +0000

    Fix

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -26,6 +26,8 @@ export default function WeatherForecast(props) {
                 <WeatherForecastPreview data={day} />
               </div>
             );
+          } else {
+            return null;
           }
         })}
       </div>
