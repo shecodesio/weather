f3b08d98db2e07bbdc0a4202935f82abe27064cd
Matt Delac <matthieu.aussaguel@gmail.com>
2022-10-17 14:53:31 +0100

    Use San Francisco as default city

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="New York" />
+        <Weather defaultCity="San Francisco" />
       </div>
     </div>
   );
