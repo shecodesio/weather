6bb2adfedb220ecb99bfaf881cf5c956424f5599
Matt Delac <matthieu.aussaguel@gmail.com>
2025-06-05 08:13:06 +0100

    Fix typos

--- src/WeatherForecastPreview.js
+++ src/WeatherForecastPreview.js
@@ -5,7 +5,7 @@ export default function WeatherForecastPreview(props) {
   function day() {
     let date = new Date(props.data.time * 1000);
     let day = date.getDay();
-    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
     return days[day];
   }
 
